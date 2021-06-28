3f047c06d6aa709ea6f1215234fec5d33c742792
Seun Adekunle <56938245+seunadekunle@users.noreply.github.com>
2021-06-28 19:03:55 -0400

    User can logout

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -71,5 +72,10 @@ public class TimelineActivity extends AppCompatActivity {
                 Log.i(TAG, "onFailure" + response, throwable);
             }
         });
+    }
+
+    public void onLogOutClicked(View view) {
+        client.clearAccessToken();  // forgets who has logged in
+        finish();
     }
 }
